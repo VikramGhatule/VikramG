9488f996477822b7bf129154f7803bdb14305b8c
Admin <Admin@DESKTOP-4MK9F1J>
2021-12-28 11:56:46 +0530

    1st commit 28 Dec 21

---
+++ GitHubTesting/src/gitCommit/GitHubCommitTest.java
@@ -0,0 +1,11 @@
+package gitCommit;
+
+public class GitHubCommitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Successfully Uploaded...!!!");
+
+	}
+
+}
