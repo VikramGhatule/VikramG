e441b2083858202cca282681115d0f87d668f66b
Admin <Admin@DESKTOP-4MK9F1J>
2021-12-28 12:00:32 +0530

    2nd commit on 28 dec 12.00pm

--- GitHubTesting/src/gitCommit/GitHubCommitTest.java
+++ GitHubTesting/src/gitCommit/GitHubCommitTest.java
@@ -5,6 +5,7 @@ public class GitHubCommitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Successfully Uploaded...!!!");
+		System.out.println("I have made some changes on this project plz refer this and pull your system.");
 
 	}
 
